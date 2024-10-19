4c8a74e290537179544a9179e052aafc5e3f9993
tad dy <a@a.com>
2024-10-19 13:53:49 -0400

    pushing changes to jwt_dependency and requirements.txt

--- src/deps.py
+++ src/deps.py
@@ -1,45 +1,36 @@
 from typing import Annotated
-from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from dotenv import load_dotenv
 import os
-from .db.database import SessionLocal
+from fastapi import Request
 
 load_dotenv()
 
 SECRET_KEY = os.getenv('AUTH_SECRET_KEY')
 ALGORITHM = os.getenv('AUTH_ALGORITHM')
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
-
-db_dependency = Annotated[Session, Depends(get_db)]
-
 bcrypt_context = CryptContext(schemes=["sha256_crypt"])
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
-oauth2_bearer_dependency = Annotated[str, Depends(oauth2_bearer)]
 
-
-async def get_current_user(token: oauth2_bearer_dependency):
+async def get_current_user(request: Request):
     try:
-        print('get_current_user', token)
+        token = request.cookies.get("jwt")
+
+        if not token:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
 
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-        user_id: int = payload.get('id')
-        if username is None or user_id is None:
+
+        email: str | None = payload.get('sub')
+        account_id: str = payload.get('id')
+        
+        if email is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
-        return {'username': username, 'id': user_id}
+        
+        return {'username': email, 'id': account_id}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
     
-
-user_dependency = Annotated[dict, Depends(get_current_user)]
+jwt_dependency = Annotated[dict, Depends(get_current_user)]
--- src/routers/huggingface/router.py
+++ src/routers/huggingface/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Response
 from sentence_transformers import SentenceTransformer
 
-from core.schemas.EmbeddingInput import EmbeddingInput
+from src.core.schemas.EmbeddingInput import EmbeddingInput
+from src.deps import jwt_dependency
 
 router = APIRouter(
     prefix='/huggingface',
@@ -9,10 +10,8 @@ router = APIRouter(
 )
 
 @router.post("/embedding")
-def create_embeddings(response: Response, textToEmbed: EmbeddingInput):
-    
+def create_embeddings(response: Response, jwt: jwt_dependency, textToEmbed: EmbeddingInput):
     embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     embeddings = embedding_model.encode(textToEmbed.input)
     embeddings_list = embeddings.tolist()
-    
     return {"embedding": embeddings_list, "model": "sentence-transformers/all-MiniLM-L6-v2"}
